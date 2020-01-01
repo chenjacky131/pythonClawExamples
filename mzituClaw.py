e44e0c25ba5a4a4c46a7bfff0bf91d77bc503668
jacky <jacky131@gmail.com>
2020-01-01 14:20:01 +0800

    1

--- mzituClaw.py
+++ mzituClaw.py
@@ -2,7 +2,7 @@ import urllib.request
 import os
 from lxml import etree
 import time
-# 请求头添加 UA
+# 请求头添加 UA referer是防止出现图片防盗链的问题
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36',
     'referer': 'https://www.mzitu.com/'
